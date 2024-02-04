69bc2dae95da50b81f7cda7de8375ae4028a4c69
johanscience <134737631+johanscience@users.noreply.github.com>
2024-02-04 17:15:48 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,4 @@
 input.onButtonPressed(Button.B, function () {
-    basic.showString("JOHAN")
-    basic.showIcon(IconNames.Square)
-    basic.showLeds(`
-        . # . # .
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
-    music.playMelody("C5 B A G F E D C ", 129)
-})
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    # . # # #
-    . # . . .
-    . # . # .
-    # . . . .
-    # # # . .
-    `)
-basic.forever(function () {
-	
+    basic.showIcon(IconNames.Heart)
+    basic.showString("Hello!")
 })
