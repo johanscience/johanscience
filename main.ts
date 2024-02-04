03ee97e3a2a1d10563ebf2774b991ae09fea9b3b
johanscience <134737631+johanscience@users.noreply.github.com>
2024-02-04 17:08:39 +0100

    johan

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
-basic.showNumber(0)
+input.onButtonPressed(Button.A, function () {
+    basic.showString("JOHAN")
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("C5 B A G F E D C ", 129)
+})
 basic.forever(function () {
 	
 })
